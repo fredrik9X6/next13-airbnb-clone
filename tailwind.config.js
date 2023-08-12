a727fcfb0a8e87538b038f79b31e29a7fbc2f289
fredrik9X6 <fredrik@9x6.se>
2023-08-12 14:21:25 +0200

    1st update

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,13 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blue: {
+          us: "#002B65",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
